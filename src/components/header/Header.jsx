dac3fe9945a37bc52a057f94ba420b729223e22c
Sevrus <book-n-troll@nordnet.fr>
2023-05-12 11:17:37 +0200

    Added headerNav component, start of header styling

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,11 @@
+import HeaderNav from "./components/headerNav/HeaderNav.jsx";
+
 const Header = () => {
 
     return (
-        <>
-
-        </>
+        <header className="header">
+            <HeaderNav />
+        </header>
     )
 }
 
