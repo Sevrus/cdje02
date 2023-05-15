3c96a0a6d918e227ea89a56a64e544ef54554c0e
Solwen02 <121789865+Solwen02@users.noreply.github.com>
2023-05-15 20:03:13 +0200

    corrected footer mobile version and done tablette version

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -7,7 +7,9 @@ import FooterRules from './components/footerRules/FooterRules.jsx';
 import FooterSocials from './components/footerSocials/FooterSocials.jsx';
 
 import footer_line_left from '../../../src/assets/lines/footer_line_left.svg';
+import footer_line_left_mobile from '../../../src/assets/lines/footer_line_left_mobile.svg';
 import footer_line_right from '../../../src/assets/lines/footer_line_right.svg';
+import footer_line_right_mobile from '../../../src/assets/lines/footer_line_right_mobile.svg';
 
 const Footer = () => {
     const mobile = window.innerWidth < 768;
@@ -15,39 +17,42 @@ const Footer = () => {
 
     if (mobile) {
         return (
-            <div className='footer'>
+            <footer className='footer'>
                 <div className='footer_top'>
                     <FooterLinks />
                 </div>
 
                 <div className='footer_center'>
-                    <img src={footer_line_left} alt="barre gauche" className="footer_line_left" />
+                    <img src={footer_line_left_mobile} alt="barre gauche" className="footer_line_left" />
                     <FooterRules />
                     <FooterSocials />
-                    <img src={footer_line_right} alt="barre droite" className="footer_line_right" />
+                    <img src={footer_line_right_mobile} alt="barre droite" className="footer_line_right" />
                 </div>
 
                 <div className='footer_bottom'>
                     <FooterCopyright />
                 </div>
-            </div>
+            </footer>
         )
-        
+
     } else if (desktop) {
         return (
-            <div className='footer'>
-                <img src={footer_line_left} alt="barre gauche" className="footer_line_left" />
-                <FooterRules />
-                <FooterSocials />
-
+            <footer className='footer'>
                 <div className='footer_center'>
+                    <div className='footer_center_left'>
+                        <FooterRules />
+                        <FooterSocials />
+                    </div>
                     <FooterLinks />
-                    <FooterCopyright />
+                    <FooterNav />
                 </div>
 
-                <FooterNav />
-                <img src={footer_line_right} alt="barre droite" className="footer_line_right" />
-            </div>
+                <div className='footer_bottom'>
+                    <img src={footer_line_left} alt="barre gauche" className="footer_line_left" />
+                    <FooterCopyright />
+                    <img src={footer_line_right} alt="barre droite" className="footer_line_right" />
+                </div>
+            </footer>
         )
     }
 }
