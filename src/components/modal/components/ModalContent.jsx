bbf460b1e3054bcaa5b25e35d16fea733d80b257
Solwen02 <121789865+Solwen02@users.noreply.github.com>
2023-05-18 00:00:34 +0200

    created modal files

---
+++ src/components/modal/components/ModalContent.jsx
@@ -0,0 +1,14 @@
+const ModalContent = ({ closeModal }) => {
+
+    return (
+        <>
+            <div className="modal__background" onClick={closeModal}>
+            </div>
+            <div className="modal__content">
+                
+            </div>
+        </>
+    )
+}
+
+export default ModalContent;
