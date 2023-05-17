34fb25d895591f6549f055b02f8c33ee7228a104
Solwen02 <121789865+Solwen02@users.noreply.github.com>
2023-05-17 18:11:49 +0200

    added confidentiality

--- src/components/policies/confidentiality/Confidentiality.jsx
+++ src/components/policies/confidentiality/Confidentiality.jsx
@@ -1,9 +1,140 @@
 const Confidentiality = () => {
 
     return (
-        <>
-            
-        </>
+        <div className="confidentiality">
+            <div className="confidentiality__header">
+                <p className="confidentiality__header__title">
+                    POLITIQUE DE CONFIDENTIALITE
+                </p>
+                <p className="confidentiality__header__site">
+                    cdje02-asso.fr
+                </p>
+                <p className="confidentiality__header__owner">
+                    Christian Bracq
+                </p>
+                <p className="confidentiality__header__type">
+                    Type de site : site informatif
+                </p>
+            </div>
+
+            <div className="confidentiality__content">
+                <div className="confidentiality__content__article">
+                    <div className="confidentiality__content__article__title">
+                        Le but de cette politique de confidentialité
+                    </div>
+                    <div className="confidentiality__content__article__content">
+                        Le but de cette politique de confidentialité est d'informer les utilisateurs de notre site des données personnelles
+                        que nous recueillerons ainsi que les informations suivantes, le cas échéant :
+                        <div className="confidentiality___content__article__content__list">
+                            <p>a. Les données personnelles que nous recueillerons</p>
+                            <p>b. L'utilisation des données recueillies</p>
+                            <p>c. Qui a accès aux données recueillies</p>
+                            <p>d. Les droits des utilisateurs du site</p>
+                            <p>e. La politique de cookies du site</p>
+                        </div>
+                        Cette politique de confidentialité fonctionne parallèlement aux conditions générales d'utilisation de notre site.
+                    </div>
+                </div>
+
+                <div className="confidentiality__content__article">
+                    <div className="confidentiality__content__article__title">
+                        Lois applicables
+                    </div>
+                    <div className="confidentiality__content__article__content">
+                        Conformément au Règlement général sur la protection des données (RGPD), cette politique de confidentialité est conforme aux règlements suivants.
+                        Les données à caractère personnel doivent être :
+                        <div className="confidentiality___content__article__content__list">
+                            <p>a. traitées de manières licite, loyale et transparente au regard de la personne concernée (licéité, loyauté, transparence);</p>
+                            <p>b. collectées pour des finalités déterminées, explicites et légitimes, et ne pas être traitées ultérieurement d'une manière incompatible avec des finalités;
+                                le traitement ultértieur à des fins archivistiques dans l'intérêt public, à des fins de recherche scientifique ou historiaue ou à des fins statistiques
+                                n'est pas considéré, conformément à l'article 89, paragraphe 1, comme incompatible avec les finalités initiales (limitation des finalités);</p>
+                            <p>c. adéquates, pertinentes et limitées à ce aui est nécessaire au regard des finalités pour lesquelles elles sont traités (minimisation des données);</p>
+                            <p>d. exactes et, si nécessaire, tenues à jour; toutes les mesures raisonnables doivent être prises pour que les données à caractères personnel aui sont inexactes,
+                                eu égard aux finalités pour lesquelles elles sont traitées, soeint effacées ou rectifiées sans tarder (exactitude);
+                            </p>
+                            <p>e. conservées sous une forme permettant l'identification des personnes concernées pendant une durée n'exédant pas celle nécessaire au regard des finalités
+                                pour lesquelles elles sont traités; les données à caractères personnel peuvent être concervées pour des durées plus longues dans la mesure où elles seront
+                                traitées exclusivement à des fins archivistiques dans l'intérêt public, à des fins de recherches scientifiques ou historiques ou à des fins statistiques
+                                conformément à l'article 89, paragraphe 1, pour autant que soient mises en oeuvre les mesures  techniques et organisationnelles appropriées requises par le règlements
+                                afin de garantir les droits et libertés de la personne concernée (limitation de la concervation);
+                            </p>
+                            <p>
+                                f. traitées de façon à garantir une sécurité apropriée des données à caractère personnel, y compris la protection contre le traitement non autoprisé ou illicite et contre la perte,
+                                la destruction ou les dégats d'origine accidentelle, à l'aide de mesures techniques ou organisationnelles appropriées (intégrité et confidentialité).
+                            </p>
+                        </div>
+
+                        Le traitement n'est licite que si, et dans la mesurte où, au moins une des conditions suivantes est remplie :
+                        <div className="confidentiality___content__article__list">
+                            <p>a. la personne concernée a consenti au traitement de ses données à caractère personnel pour une ou plusieurs finalités spécifiques ;</p>
+                            <p>b. le traitement est nécessaire au respect d'une obligation légale à laquelle le responsable du traitement est soumis ;</p>
+                            <p>c. le traitement est nécessaire à la transmission de ses données à caractère personnel pour une ou plusieurs finalités spécifiaues ;</p>
+                            <p>d. le traitement est nécessaire à la sauvegarde des intérêts vitaux de la personne concernée ou d'une autre personne physique ;</p>
+                            <p>e. le traitement est nécessaire à l'exécution d'une mission d'intérêt public ou relevant de l'exercice de l'autorité publique dont est investi le responsable du traitement ;</p>
+                            <p>f. le traitement est nécessaire aux fins des intérêts légitimes poursuivis par le responsable du traitement ou par un tiers, à moins que ne prévalent les intérêts ou les libertés
+                                et droits fondamentaux de la personne concernée qui exigent une protection des données à caractère personnel, notamment lorsque la personne concernée est un enfant.</p>
+                        </div>
+
+                        Pour les résidents de l'Etat de Californie, cette politique de confidentilité vise à se conformer à la Califormia Consumer Privacy Act (CCPA).
+                        S'il y a des incohérences entre ce document et la CCPA, la législation de l'Etat s'appliquera.
+                        Si nous constatons des incohérences, nous modifierons notre politiaue pour nous conformer à la loi pertinente.
+                    </div>
+                </div>
+
+                <div className="confidentiality__content__article">
+                    <div className="confidentiality__content__article__title">
+                        Consentement
+                    </div>
+                    <div className="confidentiality__content__article__content">
+                        Les utilisateur conviennent qu'en utilisant notre site, ils consentent à :
+                        <div className="confidentiality___content__article__content__list">
+                            <p>a. les conditions énoncées dans la présente politique de confidentialité et</p>
+                            <p>b. la collecte, l'utilisation et la conservation des données énumérées dans la présente politique.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="confidentiality__content__article">
+                    <div className="confidentiality__content__article__title">
+                        Données personnelles que nous collectons
+                    </div>
+                    <div className="confidentiality__content__article__content">
+                        Nous ne collectons, ne stockons, ni n'utilisons aucune donnée utilisateur sur notre site.
+                        Dans le cas où nous avons besoin de recueillir vos données, nous veillerons à vous fournir un préavis de ce recueil de données.
+                    </div>
+                </div>
+
+                <div className="confidentiality__content__article">
+                    <div className="confidentiality__content__article__title">
+                        Modifications
+                    </div>
+                    <div className="confidentiality__content__article__content">
+                        Cette politique de confidentialité peut être modifiée à l’occasion afin de maintenir la conformité avec la loi et de tenir compte de tout changement à notre processus de collecte de données. Nous recommandons à nos utilisateurs de vérifier notre politique de temps à autre pour s’assurer qu’ils soient informés de toute mise à jour. Au besoin, nous pouvons informer les utilisateurs par courriel des changements apportés à cette politique.
+                    </div>
+                </div>
+
+                <div className="confidentiality__content__article">
+                    <div className="confidentiality__content__article__title">
+                        Contact
+                    </div>
+                    <div className="confidentiality__content__article__content__contact">
+                        <p>Si vous avez des questions à nous poser, n'hésitez pas à communiquer avec nous en utilisant ce qui suit :</p>
+                        <p>06 64 98 13 41</p>
+                        <a href="mailto:bracq.christian.ffe@gmail.com">bracq.christian.ffe@gmail.com</a>
+                        <p>11 place du Nain d'Alsace, appt 30, 02100 ST QUENTIN</p>
+                    </div>
+                </div>
+            </div>
+
+            <div className="confidentiality__footer">
+                <div className="confidentiality__footer__date">
+                    Date d'entrée en vigueur : le 28 Juillet 2023
+                </div>
+                <div className="confidentiality__footer__copyright">
+                    © 2002-2023, DocumentsLégaux (Sequiter Inc.)
+                </div>
+            </div>
+        </div>
     )
 }
 
--- src/components/policies/legalNotice/LegalNotice.jsx
+++ src/components/policies/legalNotice/LegalNotice.jsx
@@ -1,68 +1,84 @@
 const LegalNotice = () => {
 
     return (
-        <div className="legal_notice">
-            <div className="legal_notice_header">
-                <div className="legal_notice_header_title">
+        <div className="legal-notice">
+            <div className="legal-notice__header">
+                <div className="legal-notice__header__title">
                     Mentions légales
                 </div>
-                <div className="legal_notice_header_subtitle">
+                <div className="legal-notice__header__subtitle">
                     En vigueur au 28/07/2023
                 </div>
             </div>
 
-            <div className="legal_notice_content">
-                <div className="legal_notice_content_intro">
-                    <p>Conformément aux dispositions des Articles 6-III et 19 de la Loi n°2004-575 du 21 juin 2004 pour la
+            <div className="legal-notice__content">
+                <div className="legal-notice__content__intro">
+                    <p>
+                        Conformément aux dispositions des Articles 6-III et 19 de la Loi n°2004-575 du 21 juin 2004 pour la
                         Confiance dans l’économie numérique, dite L.C.E.N., il est porté à la connaissance des utilisateurs et
                         visiteurs, ci-après l""Utilisateur", du site cdje02-asso.fr , ci-après le "Site", les présentes mentions
-                        légales.</p>
-                    <p>La connexion et la navigation sur le Site par l’Utilisateur implique acceptation intégrale et sans réserve
-                        des présentes mentions légales.</p>
-                    <p>Ces dernières sont accessibles sur le Site à la rubrique « Mentions légales ».</p>                </div>
-
-                <div className="legal_notice_content_article_title">
-                    ARTICLE 1 - L'EDITEUR
-                </div>
-                <div className="legal_notice_content_article">
-                    L’édition et la direction de la publication du Site est assurée par Bracq Christian, domiciliée 11 place
-                    du Nain d'Alsace, appt 30, 02100 ST QUENTIN, dont le numéro de téléphone est 0664981341, et
-                    l'adresse e-mail bracq.christian.ffe@gmail.com.
-                    ci-après l'"Editeur".
-                </div>
-                <div className="legal_notice_content_article_title">
-                    ARTICLE 2 - L'HEBERGEUR
-                </div>
-                <div className="legal_notice_content_article">
-                    L'hébergeur du Site est la société Ionos, dont le siège social est situé au IONOS SARL, 7 PL DE LA
-                    GARE 57200 SARREGUEMINES , avec le numéro de téléphone : 0970808911 + adresse mail de
-                    contact
+                        légales.
+                    </p>
+                    <p>
+                        La connexion et la navigation sur le Site par l’Utilisateur implique acceptation intégrale et sans réserve
+                        des présentes mentions légales.
+                    </p>
+                    <p>
+                        Ces dernières sont accessibles sur le Site à la rubrique « Mentions légales ».
+                    </p>
                 </div>
 
-                <div className="legal_notice_content_article_title">
-                    ARTICLE 3 - ACCES AU SITE
-                </div>
-                <div className="legal_notice_content_article">
-                    Le Site est accessible en tout endroit, 7j/7, 24h/24 sauf cas de force majeure, interruption
-                    programmée ou non et pouvant découlant d’une nécessité de maintenance.
-                    En cas de modification, interruption ou suspension du Site, l'Editeur ne saurait être tenu responsable.
+                <div className="legal-notice__content__article">
+                    <div className="legal-notice__content__article__title">
+                        ARTICLE 1 - L'EDITEUR
+                    </div>
+                    <div className="legal-notice__content__article__content">
+                        L’édition et la direction de la publication du Site est assurée par Bracq Christian, domiciliée 11 place
+                        du Nain d'Alsace, appt 30, 02100 ST QUENTIN, dont le numéro de téléphone est 0664981341, et
+                        l'adresse e-mail <a href="mailto:bracq.christian.ffe@gmail.com">bracq.christian.ffe@gmail.com</a>.
+                        ci-après l'"Editeur".
+                    </div>
                 </div>
 
-                <div className="legal_notice_content_article_title">
-                    ARTICLE 4 - COLLECTE DES DONNEES
+                <div className="legal-notice__content__article">
+                    <div className="legal-notice__content__article__title">
+                        ARTICLE 2 - L'HEBERGEUR
+                    </div>
+                    <div className="legal-notice__content__article__content">
+                        L'hébergeur du Site est la société Ionos, dont le siège social est situé au IONOS SARL, 7 PL DE LA
+                        GARE 57200 SARREGUEMINES , avec le numéro de téléphone : 0970808911 + adresse mail de
+                        contact
+                    </div>
                 </div>
-                <div className="legal_notice_content_article">
-                    Le site est exempté de déclaration à la Commission Nationale Informatique et Libertés (CNIL) dans la
-                    mesure où il ne collecte aucune donnée concernant les utilisateurs.
-                    1
-                    Toute utilisation, reproduction, diffusion, commercialisation, modification de toute ou partie du Site,
-                    sans autorisation de l’Editeur est prohibée et pourra entraînée des actions et poursuites judiciaires
-                    telles que notamment prévues par le Code de la propriété intellectuelle et le Code civil.
+
+                <div className="legal-notice__content__article">
+                    <div className="legal-notice__content__article__title">
+                        ARTICLE 3 - ACCES AU SITE
+                    </div>
+                    <div className="legal-notice__content__article__content">
+                        Le Site est accessible en tout endroit, 7j/7, 24h/24 sauf cas de force majeure, interruption
+                        programmée ou non et pouvant découlant d’une nécessité de maintenance.
+                        En cas de modification, interruption ou suspension du Site, l'Editeur ne saurait être tenu responsable.
+                    </div>
+                </div>
+
+                <div className="legal-notice__content__article">
+                    <div className="legal-notice__content__article__title">
+                        ARTICLE 4 - COLLECTE DES DONNEES
+                    </div>
+                    <div className="legal-notice__content__article__content">
+                        Le site est exempté de déclaration à la Commission Nationale Informatique et Libertés (CNIL) dans la
+                        mesure où il ne collecte aucune donnée concernant les utilisateurs.
+                        1
+                        Toute utilisation, reproduction, diffusion, commercialisation, modification de toute ou partie du Site,
+                        sans autorisation de l’Editeur est prohibée et pourra entraînée des actions et poursuites judiciaires
+                        telles que notamment prévues par le Code de la propriété intellectuelle et le Code civil.
+                    </div>
                 </div>
             </div>
 
-            <div className="legal_notice_footer">
-                Rédigé sur http://legalplace.fr
+            <div className="legal-notice__footer">
+                Rédigé sur <a href="http://legalplace.fr">http://legalplace.fr</a>
             </div>
         </div >
     )
