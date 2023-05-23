e47aadc36515a994eb031998276c56a9cdf6d942
Solwen02 <121789865+Solwen02@users.noreply.github.com>
2023-05-23 23:02:37 +0200

    scroll chessboard disappear effect for all versions

--- src/components/header/components/headerTitle/HeaderTitle.jsx
+++ src/components/header/components/headerTitle/HeaderTitle.jsx
@@ -9,16 +9,16 @@ const HeaderTitle = ({chessboardDisappear}) => {
               <img
                   src={rightBar}
                   alt="barre droite"
-                  className={chessboardDisappear && window.screen.width < 768 ? 'header-title--disappear-bar' : 'header-title--right-bar'}
+                  className={chessboardDisappear ? 'header-title--disappear-bar' : 'header-title--right-bar'}
               />
               <img
                   src={leftBar}
                   alt="barre gauche"
-                  className={chessboardDisappear && window.screen.width < 768 ? 'header-title--disappear-bar' : 'header-title--left-bar'}
+                  className={chessboardDisappear ? 'header-title--disappear-bar' : 'header-title--left-bar'}
               />
               <h1>CDJE<span>02</span></h1>
           </div>
-          <img src={headerImg} alt="Un échiquier" className={chessboardDisappear && window.screen.width < 768 ? 'chessboard-disappear' : 'chessboard'}/>
+          <img src={headerImg} alt="Un échiquier" className={chessboardDisappear ? 'chessboard-disappear' : 'chessboard'}/>
       </>
   )
 }
