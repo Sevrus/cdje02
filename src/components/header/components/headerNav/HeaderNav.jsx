ef7eb040a67a06a2595656764d5090638f59eb4b
Solwen02 <121789865+Solwen02@users.noreply.github.com>
2023-05-25 16:58:13 +0200

    changed scroll header, added logo, removed icons

--- src/components/header/components/headerNav/HeaderNav.jsx
+++ src/components/header/components/headerNav/HeaderNav.jsx
@@ -1,14 +1,11 @@
 import {
-    faChessKing,
-    faChessKnight,
-    faChessQueen,
-    faChessRook,
     faRightToBracket
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useEffect, useState } from "react";
 import { createPortal } from "react-dom";
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
+import logoCDJE from "../../../../assets/svg/logo_cdje.svg";
 import Modal from "../../../modal/Modal";
 
 const HeaderNav = ({ chessboardDisappear }) => {
@@ -44,20 +41,25 @@ const HeaderNav = ({ chessboardDisappear }) => {
     }, []);
 
     return (
-        <nav className="navbar">
+        <nav className={chessboardDisappear ? 'navbar--scroll' : 'navbar'}>
             <div className="navbar__burger" onClick={handleClick}>
                 <div className={burgerClass}></div>
                 <div className={burgerClass}></div>
                 <div className={burgerClass}></div>
             </div>
             {(isOpen || width > 767) && (
-                <ul className="navbar__links">
+                <ul className={chessboardDisappear ? 'navbar__links--scroll' : 'navbar__links'}>
                     <li className={chessboardDisappear ? 'navbar__links--disappear-site-name' : 'navbar__links--site-name'}>
-                        <FontAwesomeIcon icon={faChessKing} className="navbar__links--site-name__icon" />
-                        CDJE<span>02</span>
+                        <p>CDJE</p>
+                        <p className="navbar__links--site-name__span">02</p>
                     </li>
-                    <li className="navbar__links__link">
-                        <FontAwesomeIcon icon={faChessKing} className="navbar__links__link__icon" />
+
+                    <Link to={`/`}>
+                        <img src={logoCDJE} alt="logo CDJE02"
+                            className={chessboardDisappear ? 'navbar__links__logo--scroll' : 'navbar__links__logo'} />
+                    </Link>
+
+                    <li className={chessboardDisappear ? 'navbar__links__link--scroll' : 'navbar__links__link'}>
                         <NavLink
                             to={`/`}
                             className={({ isActive, isPending }) =>
@@ -67,8 +69,7 @@ const HeaderNav = ({ chessboardDisappear }) => {
                             ACCUEIL
                         </NavLink>
                     </li>
-                    <li className="navbar__links__link">
-                        <FontAwesomeIcon icon={faChessQueen} className="navbar__links__link__icon" />
+                    <li className={chessboardDisappear ? 'navbar__links__link--scroll' : 'navbar__links__link'}>
                         <NavLink
                             to={`/info`}
                             className={({ isActive, isPending }) =>
@@ -78,8 +79,7 @@ const HeaderNav = ({ chessboardDisappear }) => {
                             A PROPOS
                         </NavLink>
                     </li>
-                    <li className="navbar__links__link">
-                        <FontAwesomeIcon icon={faChessRook} className="navbar__links__link__icon" />
+                    <li className={chessboardDisappear ? 'navbar__links__link--scroll' : 'navbar__links__link'}>
                         <NavLink
                             to={`/activity`}
                             className={({ isActive, isPending }) =>
@@ -89,8 +89,7 @@ const HeaderNav = ({ chessboardDisappear }) => {
                             ACTIVITÉS
                         </NavLink>
                     </li>
-                    <li className="navbar__links__link">
-                        <FontAwesomeIcon icon={faChessKnight} className="navbar__links__link__icon" />
+                    <li className={chessboardDisappear ? 'navbar__links__link--scroll' : 'navbar__links__link'}>
                         <NavLink
                             to={`/contact`}
                             className={({ isActive, isPending }) =>
@@ -100,8 +99,9 @@ const HeaderNav = ({ chessboardDisappear }) => {
                             CONTACT
                         </NavLink>
                     </li>
-                    <li className="navbar__links__link">
-                        <FontAwesomeIcon icon={faRightToBracket} className="navbar__links__link__icon" />
+                    <li className={chessboardDisappear ? 'navbar__links__link--scroll' : 'navbar__links__link'}>
+                        <FontAwesomeIcon icon={faRightToBracket}
+                            className={chessboardDisappear ? 'navbar__links__link__icon--scroll' : 'navbar__links__link__icon'} />
                         <a onClick={() => setOpenModal(true)}>
                             LOGIN
                         </a>
--- src/components/header/components/headerTitle/HeaderTitle.jsx
+++ src/components/header/components/headerTitle/HeaderTitle.jsx
@@ -6,16 +6,10 @@ const HeaderTitle = ({ chessboardDisappear }) => {
     return (
         <>
             <div className="header-title">
-                <img
-                    src={rightBar}
-                    alt="barre droite"
-                    className={chessboardDisappear ? 'header-title--disappear-bar' : 'header-title--right-bar'}
-                />
-                <img
-                    src={leftBar}
-                    alt="barre gauche"
-                    className={chessboardDisappear ? 'header-title--disappear-bar' : 'header-title--left-bar'}
-                />
+                <img src={rightBar} alt="barre droite"
+                    className={chessboardDisappear ? 'header-title--disappear-bar' : 'header-title--right-bar'} />
+                <img src={leftBar} alt="barre gauche"
+                    className={chessboardDisappear ? 'header-title--disappear-bar' : 'header-title--left-bar'} />
                 <h1 className={chessboardDisappear ? 'header-title--cdje' : 'header-title--disappear-cdje'}>
                     CDJE<span>02</span>
                 </h1>
