225c62819801c0f4cbf447a70e0240721b46193c
anthony <shanagan77@gmail.com>
2023-05-26 14:23:22 +0200

    update form.jsx with EmailJS

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -7,31 +7,34 @@ const Form = () => {
     const sendEmail = (e) => {
         e.preventDefault();
 
-        emailjs.sendForm('contact_service', 'contact_form', form.current, 'uNJ6omJwqTvh-UN0K')
-            .then((result) => {
-                console.log(result.text);
-            }, (error) => {
-                console.log(error.text);
-            });
+        emailjs.sendForm('contact_service', 'contact_form', form.current, 'LoB492yO3zEvCzY_C')
+            .then(
+                (result) => {
+                    console.log(result.text);
+                },
+                (error) => {
+                    console.log(error.text);
+                }
+            );
     };
 
     return (
         <form ref={form} onSubmit={sendEmail} className="form">
             <div className="form__lastname">
                 <label htmlFor="lastname">Nom</label>
-                <input type="text" name="lastname" maxLength={20} pattern='/^[A-Za-z]+$/i'/>
+                <input type="text" name="lastname" maxLength={20} required={true} />
             </div>
             <div className="form__firstname">
                 <label htmlFor="firstname">Prénom</label>
-                <input type="text" name='firstname' maxLength={20} pattern='/^[A-Za-z]+$/i'/>
+                <input type="text" name='firstname' maxLength={20} required={true} />
             </div>
             <div className="form__email">
                 <label htmlFor="email">Email</label>
-                <input type="email" name='email' maxLength={40} pattern='[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$'/>
+                <input type="email" name='email' maxLength={40} required={true} />
             </div>
             <div className="form__message">
                 <label htmlFor="message">Message</label>
-                <textarea cols="30" rows="10" name='message' maxLength={200} />
+                <textarea cols="30" rows="10" name='message' maxLength={200} required={true} />
             </div>
             <div className="form__btn">
                 <button>Envoyer</button>
