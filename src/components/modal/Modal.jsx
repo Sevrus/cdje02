22312d7985d7f6c4eb4531b829c9d250bf1c2156
Solwen02 <121789865+Solwen02@users.noreply.github.com>
2023-06-07 16:36:21 +0200

    added handleSubmit to login modal

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -1,30 +1,43 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Modal = ({ closeModal }) => {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const navigate = useNavigate();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(email, password);
+        closeModal();
+        navigate('/admin');
+    }
 
     return (
         <>
             <div className="modal" onClick={closeModal}>
             </div>
-            <div className="modal__content">
+
+            <form className="modal__content" onSubmit={handleSubmit}>
                 <div className="modal__content__close" onClick={closeModal}></div>
 
                 <div className="modal__content__email">
-                    <label htmlFor="email" className="modal__content__email__label">Email</label>
-                    <input type="email" name="email" id="email" className="modal__content__email__input" />
+                    <label className="modal__content__email__label" htmlFor="email" >Email</label>
+                    <input className="modal__content__email__input" type="email" name="email" id="email" 
+                        value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Entrez votre adresse mail" required />
                 </div>
 
                 <div className="modal__content__password">
-                    <label htmlFor="password" className="modal__content__password__label">Mot de passe</label>
-                    <input type="password" name="password" id="password" className="modal__content__password__input" />
+                    <label className="modal__content__password__label" htmlFor="password">Mot de passe</label>
+                    <input className="modal__content__password__input" type="password" name="password" id="password" 
+                        value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Entrez votre mot de passe" required />
                 </div>
 
-                <Link to="/admin">
-                    <button className="modal__content__button">Se Connecter</button>
-                </Link>
-                
+                <button type="submit" className="modal__content__button">Se Connecter</button>
+
                 <div className="modal__content__lost-password">Mot de passe oublié ?</div>
-            </div>
+            </form>
         </>
     )
 }
