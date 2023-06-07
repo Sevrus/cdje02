0664b4b7792d12185cc2e7461f2da20d9188ae55
anthony <shanagan77@gmail.com>
2023-06-07 15:15:49 +0200

    add modalResult.jsx and your sass

--- src/components/admins/adminResults/components/ResultDescription/ResultDescription.jsx
+++ src/components/admins/adminResults/components/ResultDescription/ResultDescription.jsx
@@ -2,8 +2,24 @@ import dataResult from "./dataResult.js"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPencil } from '@fortawesome/free-solid-svg-icons'
 import { faTrash } from '@fortawesome/free-solid-svg-icons'
+import { createPortal } from "react-dom"
+import ModalResult from "./ModalResult.jsx"
+import { useState } from "react"
 
 const ResultDescription = () => {
+    const [openModal, setOpenModal] = useState(false);
+    const [dataResults, setDataResults] = useState([])
+
+    // useEffect(() => {
+    //     http({ url: "/Results" })
+    //         .then(json => setDataResults(json))
+    // })
+
+    const handleDelete = (id) => {
+        const temp = dataResults.filter(results => results.id !== id);
+        setDataResults(temp);
+        // http({ url: "/Results/" + id, method: 'DELETE' });
+    }
 
     return (
 
@@ -19,8 +35,16 @@ const ResultDescription = () => {
 
 
                     <div className="articleResult__icon">
-                        <FontAwesomeIcon className="articleResult__icon__pencil" icon={faPencil} />
-                        <FontAwesomeIcon className="articleResult__icon__trash" icon={faTrash} />
+                        <a onClick={() => setOpenModal(true)}>
+                            <FontAwesomeIcon className="articleResult__icon__pencil" icon={faPencil} />
+                        </a>
+                        {openModal && createPortal(
+                            <ModalResult closeModal={() => setOpenModal(false)} />, document.body
+                        )}
+
+                        <span onClick={() => { handleDelete(item.id) }}>
+                            <FontAwesomeIcon className="articleResult__icon__trash" icon={faTrash} />
+                        </span>
                     </div>
 
                 </section>
