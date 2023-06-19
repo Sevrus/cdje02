3110ff34b1d04716fc77e94be89e124f401d0daf
anthony <shanagan77@gmail.com>
2023-06-19 14:05:28 +0200

    creation fetchForAll.js

---
+++ src/utilities/fetchForAll.js
@@ -0,0 +1,15 @@
+function fetchForAll(setIsLoaded, setError, setItems, url) {
+
+    fetch(url)
+        .then(res => res.json())
+        .then(
+            (result) => {
+                setIsLoaded(true);
+                setItems(result);
+            },
+            (error) => {
+                setIsLoaded(true);
+                setError(error);
+            }
+        )
+}
