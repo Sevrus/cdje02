92125db9f20d727631a01c5448e9595233d1ab56
anthony <shanagan77@gmail.com>
2023-06-28 16:50:17 +0200

    update verifPasword

--- src/components/resetPassword/RequestResetPassword.jsx
+++ src/components/resetPassword/RequestResetPassword.jsx
@@ -1,23 +1,31 @@
 import emailjs from "@emailjs/browser";
-import { useState } from "react";
-
+import { useEffect, useRef, useState } from "react";
+import fetch from "../../utilities/fetchForAll"
 
 const RequestResetPassword = () => {
 
     const [email, setEmail] = useState('');
     const [message, setMessage] = useState('');
+    const form = useRef();
+
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [datas, setDatas] = useState([]);
+
+    useEffect(() => {
+        fetch(setIsLoaded, setError, setDatas, "api/users")
+    }, [])
+
+    console.log(datas.data);
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        let email = document.getElementById("email").value;
 
         if (email === "") {
             setMessage("Veuillez entrer votre adresse mail");
 
-        } else {
-            setMessage("");
-
-            emailjs.sendForm('password_service', 'reset_password', form.current, 'LoB492yO3zEvCzY_C')
+        } if (email === datas.data) {
+            emailjs.sendForm('contact_service', 'reset_password', form.current, 'LoB492yO3zEvCzY_C')
                 .then(
                     (result) => {
                         console.log(result.text);
@@ -26,19 +34,21 @@ const RequestResetPassword = () => {
                         console.log(error.text);
                     }
                 );
-        };
+        }
+        // } else {
+        //     setMessage("Veuillez entrer une adresse mail valide")
+        // }
     };
 
-
     return (
-        <form className="requestResetPassword" onSubmit={handleSubmit}>
+        <form ref={form} onSubmit={handleSubmit} className="requestResetPassword">
 
             <div className="requestResetPassword__email">
                 <label className="requestResetPassword__email__label" htmlFor="email" >
                     Un email vous sera envoyé pour changer votre mot de passe
                 </label>
                 <input className="requestResetPassword__email__input" type="email" name="email" id="email"
-                    value={email} onChange={(e) => setEmail(e.target.value)} placeholder="adresse mail"/>
+                    value={email} onChange={(e) => setEmail(e.target.value)} placeholder="adresse mail" />
             </div>
 
             <p className="requestResetPassword__message">{message}</p>
