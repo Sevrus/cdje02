f7f73d4709d9fd8df5b358968d37cb44cc14b11a
Solwen02 <121789865+Solwen02@users.noreply.github.com>
2023-06-28 17:34:20 +0200

    animation on login modal

--- src/components/modal/LoginModal.jsx
+++ src/components/modal/LoginModal.jsx
@@ -1,10 +1,10 @@
 import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { AnimatePresence, motion } from "framer-motion";
 import { useContext, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { AuthContext } from "../../utilities/AuthContext.jsx";
 import { clearErrorAfterDelay } from "../../utilities/clearErrorAfterDelay.js";
-
 
 const LoginModal = ({ closeModal }) => {
     const { login } = useContext(AuthContext);
@@ -44,7 +44,7 @@ const LoginModal = ({ closeModal }) => {
                         login();
                         navigate("/admin");
                     } else {
-                        setError('Mot de passe incorrect.');
+                        setError('Mot de passe incorrect');
                         clearErrorAfterDelay(setError, 3000);
                     }
                 })
@@ -63,45 +63,67 @@ const LoginModal = ({ closeModal }) => {
 
     return (
         <>
-            <div className="modal" onClick={closeModal}>
-            </div>
 
-            <form className="modal__content" onSubmit={handleSubmit}>
-                <div className="modal__content__close" onClick={closeModal}></div>
+            <AnimatePresence mode="wait" onExitComplete={() => null}>
+                <div className="modal" onClick={closeModal}>
+                    <motion.div className="modal__content"
+                        initial={{ opacity: 0, scale: 0.7, x: "-50%" }}
+                        animate={{ opacity: 1, scale: 1, x: "-50%" }}
+                        transition={{
+                            duration: 0.3,
+                            ease: [0, 0.71, 0.2, 1.01],
+                            scale: {
+                                type: "spring",
+                                damping: 5,
+                                stiffness: 100,
+                            }
+                        }}
+                        exit={{
+                            opacity: 0,
+                            y: "100vh",
+                            transition: {
+                                duration: 0.5
+                            }
+                        }}>
+                        <form onSubmit={handleSubmit} onClick={(e) => e.stopPropagation()}>
+                            <div className="modal__content__close" onClick={closeModal}></div>
 
-                <div className="modal__content__email">
-                    <label className="modal__content__email__label" htmlFor="email" >Email</label>
-                    <input className="modal__content__email__input" type="email" name="email" id="email"
-                        value={email} onChange={(e) => setEmail(e.target.value)} />
+                            <div className="modal__content__email">
+                                <label className="modal__content__email__label" htmlFor="email" >Email</label>
+                                <input className="modal__content__email__input" type="email" name="email" id="email"
+                                    value={email} onChange={(e) => setEmail(e.target.value)} />
+                            </div>
+
+                            <div className="modal__content__password">
+                                <label className="modal__content__password__label" htmlFor="password">Mot de passe</label>
+                                <div>
+                                    <input className="modal__content__password__input" name="password" id="password"
+                                        type={showPassword ? "text" : "password"}
+                                        pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$"
+                                        value={password} onChange={(e) => setPassword(e.target.value)} />
+                                    <FontAwesomeIcon icon={showPassword ? faEye : faEyeSlash} className="modal__content__password__icon"
+                                        onClick={toggleShowPassword} />
+                                </div>
+                            </div>
+
+                            <p className="modal__content__message">{message}</p>
+                            {error &&
+                                <p className="modal__content__message">{error}</p>
+                            }
+
+                            <button
+                                type="submit"
+                                className="modal__content__button"
+                                disabled={isLoading}
+                            >
+                                {isLoading ? "Connexion..." : "Se connecter"}
+                            </button>
+
+                            <Link to={"/request-reset-password"} onClick={closeModal} className="modal__content__lost-password">Mot de passe oublié ?</Link>
+                        </form>
+                    </motion.div>
                 </div>
-
-                <div className="modal__content__password">
-                    <label className="modal__content__password__label" htmlFor="password">Mot de passe</label>
-                    <div>
-                        <input className="modal__content__password__input" name="password" id="password"
-                            type={showPassword ? "text" : "password"}
-                            pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$"
-                            value={password} onChange={(e) => setPassword(e.target.value)} />
-                        <FontAwesomeIcon icon={showPassword ? faEye : faEyeSlash} className="modal__content__password__icon"
-                            onClick={toggleShowPassword} />
-                    </div>
-                </div>
-
-                <p className="modal__content__message">{message}</p>
-                {error &&
-                    <p className="modal__content__message">{error}</p>
-                }
-
-                <button
-                    type="submit"
-                    className="modal__content__button"
-                    disabled={isLoading}
-                >
-                    {isLoading ? "Connexion..." : "Se connecter"}
-                </button>
-
-                <Link to={"/request-reset-password"} onClick={closeModal} className="modal__content__lost-password">Mot de passe oublié ?</Link>
-            </form>
+            </AnimatePresence >
         </>
     )
 }
